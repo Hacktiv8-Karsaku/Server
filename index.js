44ffa17d49f996c5543a0514f473f92f154422a5
AbdulRidhoRamadhan <abdulridhoramadhan.it@gmail.com>
2024-11-27 19:47:31 +0700

    feat: create task model & task schema

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ require("dotenv").config();
 const { ApolloServer } = require("@apollo/server");
 const { startStandaloneServer } = require("@apollo/server/standalone");
 const { usersResolvers, usersTypeDefs } = require("./schema/userSchema");
+const { taskResolvers, taskTypeDefs } = require("./schema/TaskSchema");
 const { verifyToken } = require("./helpers/jwt");
 const UserModel = require("./models/userModel");
 const { ObjectId } = require("mongodb");
 
 const server = new ApolloServer({
-  typeDefs: [usersTypeDefs],
-  resolvers: [usersResolvers],
+  typeDefs: [usersTypeDefs, taskTypeDefs],
+  resolvers: [usersResolvers, taskResolvers],
   introspection: true,
 });
 
